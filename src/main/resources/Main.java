f5697a4887b6d250241fcaf8d441ae329b769ecb
Gian Carlo C. Nonato <s2100676@usls.edu.ph>
2025-05-21 10:09:17 +0800

    add message dialog error

---
+++ src/main/resources/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import view.MainFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Set look and feel to system default
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Launch the application
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame mainFrame = new MainFrame();
+                mainFrame.setVisible(true);
+            }
+        });
+    }
+}
